ea54a3b7474e85ce03015a5cf41a3a0b9c4ec8ea
Hai Le <haithanh.le@kinesso.com>
2022-11-25 14:15:47 +0700

    initial setup

---
+++ src/main/java/com/hailt/parkingserviceapi/ParkingServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.hailt.parkingserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hailt/parkingserviceapi/ParkingServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hailt.parkingserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
